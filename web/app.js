1b3d5c94902150742006b7b37cfa9838e45eb187
hyx <cuphyx@163.com>
2016-10-21 00:10:09 +0800

    chagne engine

--- web/app.js
+++ web/app.js
@@ -4,15 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
+
 
 var index = require('./routes/index');
 //var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// 模板引擎
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
