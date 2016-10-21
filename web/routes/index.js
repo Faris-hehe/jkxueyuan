a326333b13a0524f43b0f4263eecb03ddbbb847c
hyx <cuphyx@163.com>
2016-10-22 00:28:23 +0800

    up

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '简快学院' });
+  res.render('index', { name: '首页' });
+});
+
+router.get('/biunique', function(req, res, next) {
+  res.render('biunique', { name: '简快学院' });
+});
+
+router.get('/teacher', function(req, res, next) {
+  res.render('teacher', { name: '师资教研' });
+});
+
+router.get('/student', function(req, res, next) {
+  res.render('student', { name: '成功学员' });
+});
+
+router.get('/xueshuo', function(req, res, next) {
+  res.render('xueshuo', { name: '师资教研' });
+});
+
+router.get('/zhuanshuo', function(req, res, next) {
+  res.render('zhuanshuo', { name: '师资教研' });
+});
+
+router.get('/english', function(req, res, next) {
+  res.render('english', { name: '师资教研' });
+});
+
+router.get('/politics', function(req, res, next) {
+  res.render('politics', { name: '师资教研' });
 });
 
 module.exports = router;
