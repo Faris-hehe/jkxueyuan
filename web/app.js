22bc1cd52304276734530f31fa1063edb023c47d
hyx <cuphyx@163.com>
2016-10-21 15:42:52 +0800

    update

--- web/app.js
+++ web/app.js
@@ -5,29 +5,31 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
-
-
-var index = require('./routes/index');
-//var users = require('./routes/users');
+var fs = require('fs');
 
 var app = express();
 
+// 错误日志
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+app.use(logger('combined', {stream: accessLogStream}));
 // 模板引擎
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// 处理favicon.ico
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+// 设置环境变量
+app.set('env', process.env.NODE_ENV || 'development');
+// 其他中间件
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 路由
+var index = require('./routes/index');
 app.use('/', index);
-//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
